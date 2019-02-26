17892c6a866fc11ffae690cccc95fe4a91acae32
Lukas Zielinski <lukas.zielinski.extern@starfinanz.de>
2019-02-26 13:44:45 +0100

    Initial Commit

---
+++ CatFactsWithPromiseKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatFactsWithPromiseKit
+//
+//  Created by Lukas Zielinski (extern) on 26.02.19.
+//  Copyright © 2019 Lukas Zielinski (extern). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
